fe6e0817b5e05293fa00d42892a4a584389d1a7f
Miller Roman <Miller.roman@mymail.champlain.edu>
2021-03-14 14:29:59 -0400

    Minor tag  changes

--- GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Scripts/Plugins/gproClientPlugin.cs
+++ GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Scripts/Plugins/gproClientPlugin.cs
@@ -8,6 +8,8 @@ public class gproClientPlugin
 {
     [DllImport("gpro-net-Client-Plugin")]
     public static extern int foo(int bar);
+    [DllImport("gpro-net-Client-Plugin")]
     public static extern void ClientSelectionMade(int x, int y);
+    [DllImport("gpro-net-Client-Plugin")]
     public static extern void SendMessages();
 }
