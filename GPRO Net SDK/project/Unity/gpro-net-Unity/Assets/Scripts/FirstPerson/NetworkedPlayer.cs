7d21af3a16285ba5d8a22b15f0fa54155cafa214
Miller Roman <Miller.roman@mymail.champlain.edu>
2021-04-25 15:24:43 -0400

    Adding locked files

---
+++ GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Scripts/FirstPerson/NetworkedPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedPlayer : MonoBehaviour
+{
+    private PlayerMove move;
+    private Transform gun;
+
+    public void NetworkUpdate(PlayerMove newMove)
+    {
+        move = newMove;
+        transform.position = new Vector3(move.LocX, move.LocY, move.LocZ);
+        gun.rotation = new Quaternion(move.RotX, move.RotY, move.RotZ, move.RotW);
+
+    }
+}
