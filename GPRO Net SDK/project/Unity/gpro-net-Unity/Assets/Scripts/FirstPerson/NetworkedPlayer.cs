e72d217fa975b099310910257035bfd63c796f17
Miller Roman <Miller.roman@mymail.champlain.edu>
2021-04-25 15:35:09 -0400

    Fixed gun transform

--- GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Scripts/FirstPerson/NetworkedPlayer.cs
+++ GPRO Net SDK/project/Unity/gpro-net-Unity/Assets/Scripts/FirstPerson/NetworkedPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,13 @@ public class NetworkedPlayer : MonoBehaviour
 {
     private PlayerMove move;
     private Transform gun;
+
+
+    private void Start()
+    {
+
+        gun = transform.GetChild(0);
+    }
 
     public void NetworkUpdate(PlayerMove newMove)
     {
